b48bf78bc34c9a800866f02a508ea90f3fde3044
Robert Pliszka <robplisz@gmail.com>
2023-05-02 19:31:55 -0400

    Figured out what was causing the issue with the styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,9 +5,9 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
+      <div className="expense-item-description">
         <h2>{props.name}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item-price">$ {props.amount}</div>
       </div>
     </div>
   );
