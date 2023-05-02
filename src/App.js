2112d6bd14571d1d8fb865700a2770a4de6f18cc
Robert Pliszka <robplisz@gmail.com>
2023-05-02 19:30:06 -0400

    Fixed the display issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,10 +5,10 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div>
+      <div className="expense-item__description">
         <h2>{props.name}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <div>$ {props.amount}</div>
     </div>
   );
 }
